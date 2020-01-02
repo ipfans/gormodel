190c26a65e5ca44471ffa7f7181b60fd6c28164a
ipfans <ipfanscn@gmail.com>
2020-01-02 18:19:08 +0800

    feat: Example for package usage.

--- base_test.go
+++ base_test.go
@@ -1,1 +1,21 @@
-package gormodel
+package gormodel_test
+
+import (
+	"github.com/ipfans/gormodel"
+)
+
+func Example() {
+	type User struct {
+		gormodel.Model
+
+		Name string
+	}
+
+	type UserRepository struct {
+		gormodel.Base
+	}
+
+	type UserRepo interface{
+		gormodel.IBase
+	}
+}
